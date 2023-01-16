dd40fb7a63e49b4f000649818941b042d2e69e03
Vladislav <vladgm233223@gmail.com>
2023-01-16 23:26:03 +0200

    add data from api
    Получил данные из апи и отобразил в компоненте

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,41 @@
 import { Search } from "../components/Search";
 import { Results } from "../components/Results";
 import { PostCard } from "../components/PostCard";
+
+import { selectAllPosts, selectPostsInfo } from "../store/posts/posts-selectors";
+import { loadPosts } from "../store/posts/posts-actions";
+
 import { Grid } from "@mui/material";
 
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Loader } from "../components/Loader";
 
 export const HomePage = () => {
-  
-    
+  const dispatch = useDispatch();
+
+  const posts = useSelector(selectAllPosts)
+
+  const { status, error, qty } = useSelector(selectPostsInfo);
+
+  useEffect(() => {
+    !qty && dispatch(loadPosts());
+  }, [qty, dispatch]);
+
   return (
     <>
       <Search />
       <Results />
-      <Grid container spacing={2} sx={{mt:"45px"}}  >
 
-      {[1,2,3].map((post) => (
-        
-        <PostCard key={post} />
-      ))}
-      </Grid>
+      {error && <h2>Can't fetch data</h2>}
+      {status === "loading" && <Loader />}
+      {status === "received" && (
+        <Grid container spacing={2} sx={{ mt: "45px" }}>
+          {posts.map((post) => (
+            <PostCard key={post.id} {...post} />
+          ))}
+        </Grid>
+      )}
     </>
   );
 };
