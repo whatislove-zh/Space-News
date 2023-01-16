5e95385f963a1e295e968f4cabe5c39f6e27d090
Vladislav <vladgm233223@gmail.com>
2023-01-16 23:27:36 +0200

    add data from api
    изменил заглушку на данные из api

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,15 @@
 import { Divider, Typography } from '@mui/material'
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectPostsInfo } from '../store/posts/posts-selectors';
 
 export const Results = () => {
+
+  const { qty } = useSelector(selectPostsInfo);
+
   return (
     <>
-    <Typography>Result {[].length}</Typography>
+    <Typography>Result {qty}</Typography>
     <Divider />
     </>
   )
