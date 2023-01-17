9c8924f2e76d2527b3c3621b59c6ec3a8cc0addb
Vladislav <vladgm233223@gmail.com>
2023-01-17 16:35:27 +0200

    Update config.js
    добавил ссылку для поиска страницы по id и увеличил лимит загружаемых постов до сотни

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 const BASE_URL = "https://api.spaceflightnewsapi.net/v3/articles"
 
-export const ALL_POSTS = BASE_URL + "?_limit=15"
+export const ALL_POSTS = BASE_URL + "?_limit=100"
+
+export const searchPostById = (id) => BASE_URL + `/${id}`
 
