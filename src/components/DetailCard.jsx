4f0f603157751c21feb5f121f5bb0329bc7b01c0
Vladislav <vladgm233223@gmail.com>
2023-01-17 16:40:22 +0200

    Create DetailCard.jsx

---
+++ src/components/DetailCard.jsx
@@ -0,0 +1,61 @@
+import { autocompleteClasses, Box, Typography } from "@mui/material";
+import React from "react";
+import { Container } from "@mui/system";
+import zIndex from "@mui/material/styles/zIndex";
+import { BackToHomeBtn } from "./BackToHomeBtn";
+
+export const DetailCard = (props) => {
+  const { title, imageUrl, summary } = props;
+
+  return (
+    <>
+      <Box width={"100%"} height="245px">
+        <img
+          className="detail_img"
+          src={imageUrl}
+          alt={title}
+          width={"100%"}
+          height="100%"
+        />
+      </Box>
+      <Container
+        sx={{
+          mt: "50px",
+          maxWidth: "1290px",
+          border: "1px solid #EAEAEA",
+          boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.5)",
+          position: "relative",
+          top: "-150px",
+          background: "#ffffff",
+          borderRadius: "5px",
+        }}
+      >
+        <Typography
+          variant="h2"
+          fontSize={24}
+          width="max-content"
+          sx={{ m: "35px auto 50px" }}
+        >
+          {title}
+        </Typography>
+        <Typography
+          variant="body1"
+          fontSize={18}
+          component={"p"}
+          sx={{ mb: "50px" }}
+        >
+          {summary.repeat(10)}
+        </Typography>
+      </Container>
+      <Container
+        sx={{
+          position: "relative",
+          top: "-150px",
+          mt: "35px",
+        }}
+      >
+        <BackToHomeBtn />
+      </Container>
+    </>
+  );
+};
