3275a2916036f9afbd5589eaa3e95b75e3d3cd5b
Vladislav <vladgm233223@gmail.com>
2023-01-17 16:38:37 +0200

    add calendar and navigate to detail page

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -8,29 +8,44 @@ import {
   Grid,
 } from "@mui/material";
 
+import { useNavigate } from "react-router-dom";
+
 import CalendarMonthSharpIcon from "@mui/icons-material/CalendarMonthSharp";
 import { Box } from "@mui/system";
 
+
 export const PostCard = (props) => {
-  const { title, imageUrl, summary, publishedAt } = props;
+  const { title, imageUrl, summary, publishedAt, id } = props;
+
+  const navigate = useNavigate();
 
   const [month, day, year] = new Date(publishedAt)
     .toLocaleString("en-us", { day: "numeric", month: "long", year: "numeric" })
     .split(" ");
-  const publishedDate = month + day.replace(",", "th,") + year;
+
+  const publishedDate = month + " " + day.replace(",", "th, ") + year;
 
   return (
     <Grid item xs={12} md={4} justifyContent="center">
       <Card sx={{ maxWidth: 400, margin: "auto", height: "530px" }}>
         <CardMedia
+          onClick={() => navigate(`/posts/${id}`)}
           component="img"
-          alt="green iguana"
+          alt={title}
           height="217px"
           image={imageUrl}
+          sx={{ cursor: "pointer" }}
         />
         <CardContent>
-          <Box display={"flex"} alignItems={"center"}>
-            <CalendarMonthSharpIcon color={"#363636"} sx={{ opacity: 0.6, mr:"8px" }} />
+          <Box
+            display={"flex"}
+            alignItems={"center"}
+            sx={{ mb: "20px", mt: "10px" }}
+          >
+            <CalendarMonthSharpIcon
+              color={"#363636"}
+              sx={{ opacity: 0.6, mr: "8px" }}
+            />
             <Typography
               color={"#363636"}
               sx={{ opacity: 0.6 }}
@@ -43,8 +58,9 @@ export const PostCard = (props) => {
           <Typography
             gutterBottom
             variant="h6"
-            sx={{ height: "58px", mb: "1rem" }}
+            sx={{ height: "58px", mb: "1rem", cursor: "pointer" }}
             component="p"
+            onClick={() => navigate(`/posts/${id}`)}
           >
             {title.substr(0, 52) + "..."}
           </Typography>
@@ -58,7 +74,9 @@ export const PostCard = (props) => {
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small">Read More</Button>
+          <Button size="small" onClick={() => navigate(`/posts/${id}`)}>
+            Read More
+          </Button>
         </CardActions>
       </Card>
     </Grid>
