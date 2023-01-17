29b82bfe83373bd18a06ec24f3f2214b4259dcaf
Vladislav <vladgm233223@gmail.com>
2023-01-17 16:33:24 +0200

    little style and backhome btn
    добавил немного стилей для страницы Not Found и добавил кнопку для возврата на предыдущую страницу

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,20 @@
 import { Typography } from "@mui/material";
+import { Container, flexbox } from "@mui/system";
+import { BackToHomeBtn } from "../components/BackToHomeBtn";
 
 export const NotFound = () => {
   return (
-    <Typography variant="h1" gutterBottom>
-      This page doesn't exist
-    </Typography>
+    <Container sx={{
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"
+    }}>
+      <Typography variant="h1" gutterBottom>
+        This page doesn't exist
+      </Typography>
+      <BackToHomeBtn/>
+    </Container>
   );
 };
