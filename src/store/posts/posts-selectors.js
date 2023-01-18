a859ac26190bf35c822df32a41de4320e69498d8
Vladislav <vladgm233223@gmail.com>
2023-01-18 11:03:08 +0200

    Update posts-selectors.js
    изменил алгоритм селектора для отбора постов по содержимому строки поиска

--- src/store/posts/posts-selectors.js
+++ src/store/posts/posts-selectors.js
@@ -1,7 +1,15 @@
 export const selectPostsInfo = (state) => ({
-    status: state.posts.status,
-    error: state.posts.error,
-    qty: state.posts.list.length,
-})
+  status: state.posts.status,
+  error: state.posts.error,
+  qty: state.posts.list.length,
+});
 
-export const selectAllPosts = (state) => state.posts.list
+export const selectAllPosts = (state) => state.posts.list;
+
+export const selectVisiblePosts = (state, { search = "" }) => {
+  return state.posts.list.filter(
+    (post) =>
+      post.title.toLowerCase().includes(search.toLowerCase()) ||
+      post.summary.toLowerCase().includes(search.toLowerCase())
+  );
+};
