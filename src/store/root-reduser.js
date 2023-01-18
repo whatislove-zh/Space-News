0d82567561c9a271fe636ba50b0499bb71019495
Vladislav <vladgm233223@gmail.com>
2023-01-18 11:02:01 +0200

    redux element for searching posts
    создал actions, reduser, selector для поиска постов по полю ввода

--- src/store/root-reduser.js
+++ src/store/root-reduser.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { controlsReduser } from "./controls/controls-reduser";
 import { detailsReduser } from "./details/details-reduser";
 import { postsReduser } from "./posts/posts-reduser";
 
 export const rootReduser = combineReducers({
     posts: postsReduser,
     details: detailsReduser,
+    controls: controlsReduser,
 })
