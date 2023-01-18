b86c9bd5cc894036c03bf127f0152d30703a2c7b
Vladislav <vladgm233223@gmail.com>
2023-01-18 11:04:21 +0200

    fix result
    теперь в результатах актуальное число постов

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,11 +1,19 @@
 import { Divider, Typography } from '@mui/material'
 import React from 'react'
 import { useSelector } from 'react-redux';
-import { selectPostsInfo } from '../store/posts/posts-selectors';
+
+import { selectVisiblePosts } from '../store/posts/posts-selectors';
+import { selectSearch } from '../store/controls/controls-selectors';
 
 export const Results = () => {
 
-  const { qty } = useSelector(selectPostsInfo);
+  const search = useSelector(selectSearch)
+  
+  const posts = useSelector((state) => (
+    selectVisiblePosts(state, {search})
+  ))
+
+  const qty = posts.length
 
   return (
     <>
